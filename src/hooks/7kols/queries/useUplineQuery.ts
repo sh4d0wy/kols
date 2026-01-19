61d56a9f33dab5d599d8251bb4de009b2b818163
sh4d0wy <sakshambhugra8@gmail.com>
2026-01-19 16:02:04 +0530

    fix: fixed build errors

--- src/hooks/7kols/queries/useUplineQuery.ts
+++ src/hooks/7kols/queries/useUplineQuery.ts
@@ -2,7 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import { use7KolsContract } from "@/hooks/use7KolsContract";
 import { useConnection } from "wagmi";
 import { useCallback } from "react";
-import { formatUnits, isAddress } from "viem";
+import { isAddress } from "viem";
 
 export const useUplineQuery = () => {
     const {readContract} = use7KolsContract();
